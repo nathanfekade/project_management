9c0e5c34246913876eaf3f67a2b602b2ae91ece0
nathanfekade <nathanfekade276@gmail.com>
2025-03-26 12:30:46 +0300

    a single user can't have the same title name or category name

--- task/models.py
+++ task/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Task(models.Model):
 
-    STATUS_CHOICES = [('to-do','TO-DO'), ('in progress', 'In Progress'), ('done', 'Done')]
+    STATUS_CHOICES = [('todo','TO-DO'), ('inprogress', 'In Progress'), ('done', 'Done')]
     PRIORITY_CHOICES = [('low','Low'), ('medium', 'Medium'), ('high', 'High')]
 
     title = models.CharField(max_length=100)
@@ -10,10 +11,24 @@ class Task(models.Model):
     deadline = models.DateTimeField()
     status = models.CharField(max_length=30, choices=STATUS_CHOICES, default="to-do")
     priority = models.CharField(max_length=30, choices=PRIORITY_CHOICES, default="low")
+    user = models.ForeignKey(User, related_name='task_user', on_delete=models.CASCADE)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['title','user'], name='unique_task_user')
+        ]  
+
 
 class Category(models.Model):
    
     title = models.CharField(max_length=100)
     tasks = models.ManyToManyField(Task, related_name='categories')
+    user = models.ForeignKey(User, related_name='category_user', on_delete=models.CASCADE)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['title','user'], name='unique_category_user')
+        ]    
+
 
     
