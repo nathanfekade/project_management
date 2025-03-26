0477d9b3fe0acb1754f805a5f77fdd959bbcc570
nathanfekade <nathanfekade276@gmail.com>
2025-03-26 10:19:49 +0300

    created task and category model

--- task/models.py
+++ task/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+
+    STATUS_CHOICES = [('to-do','TO-DO'), ('in progress', 'In Progress'), ('done', 'Done')]
+    PRIORITY_CHOICES = [('low','Low'), ('medium', 'Medium'), ('high', 'High')]
+
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    deadline = models.DateTimeField()
+    status = models.CharField(max_length=30, choices=STATUS_CHOICES, default="to-do")
+    priority = models.CharField(max_length=30, choices=PRIORITY_CHOICES, default="low")
+
+class Category(models.Model):
+   
+    title = models.CharField(max_length=100)
+    tasks = models.ManyToManyField(Task, related_name='categories')
+
+    
