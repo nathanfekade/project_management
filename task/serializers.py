caa7a483b38be4f9d06d0433b5ea55ee46f8ce83
nathanfekade <nathanfekade276@gmail.com>
2025-04-03 13:00:32 +0300

    overriding create and update method to show user friendly error when unique constraint is violated

--- task/serializers.py
+++ task/serializers.py
@@ -6,3 +6,27 @@ class TaskSerializer(serializers.ModelSerializer):
         model = Task
         fields = '__all__'
         read_only_fields = ('user',)
+    
+    def create(self, validated_data):
+        task = Task(**validated_data)
+
+        try:
+            task.full_clean()
+        except serializers.ValidationError as e:
+            raise
+
+        task.save()
+        return task
+    
+    def update(self, instance, validated_data):
+        
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        try:
+            instance.full_clean()
+        except serializers.ValidationError as e:
+            raise
+
+        instance.save()
+        return instance
